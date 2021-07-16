f8c8c696ed8da55425307d135cc91258168566bc
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-16 10:54:02 +0900

    🚨 lintエラーの解消

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from "next-themes";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps)  => {
   return (
     <ThemeProvider attribute='class'>
       <Component {...pageProps} />
