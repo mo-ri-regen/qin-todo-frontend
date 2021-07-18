6df81679cee3a04ad8af13ea48c3e24d8cce0641
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-18 20:39:17 +0900

    Themeの切り替え（仮）機能を追加

---
+++ src/components/ThemeChanger.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+
+export const ThemeChanger = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return (
+    <div className="mt-12">
+      {theme !== undefined && (
+        <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+          <option value='dark'>Dark</option>
+          <option value='light'>Light</option>
+          <option value='system'>System</option>
+        </select>
+      )}
+    </div>
+  );
+};
