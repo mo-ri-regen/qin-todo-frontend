c14da296f0a0bf6f382132365054ffd2ebeb5041
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-19 13:22:32 +0900

    🚨 lintエラーの解消

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 import { ThemeChanger } from "src/components/ThemeChanger";
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <div className='text-5xl text-blue-500'>Qin Todo</div>
       <ThemeChanger/>
     </>
   );
-=======
-const Home = () => {
-  return <div className='text-5xl text-blue-500'>Qin Todo</div>;
->>>>>>> 882f768 (🚨 lint エラーの解消)
 }
 
 export default Home
