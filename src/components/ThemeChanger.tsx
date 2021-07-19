e30c6d380cf662b94ae60a33bb8d6c25420cae08
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-07-19 15:42:41 +0900

    🐛 構文エラーを修正

--- src/components/ThemeChanger.tsx
+++ src/components/ThemeChanger.tsx
@@ -5,11 +5,10 @@ export const ThemeChanger = () => {
   const [isMounted, setIsMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  const handleOnChange = () => {
-    (e:any) => {
-      return setTheme(e.target.value);
-    };
+  const handleOnChange = (e: any) => {
+    setTheme(e.target.value);
   };
+
   // When mounted on client, now we can show the UI
   useEffect(() => {
     setIsMounted(true);
@@ -20,7 +19,7 @@ export const ThemeChanger = () => {
   return (
     <div className='mt-12'>
       {theme !== undefined && (
-        <select value={theme} onBlur={handleOnChange}>
+        <select value={theme} onBlur={handleOnChange} onChange={handleOnChange}>
           <option value='dark'>Dark</option>
           <option value='light'>Light</option>
           <option value='system'>System</option>
