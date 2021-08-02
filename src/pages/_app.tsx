ffa380de4cd2516ab81f83435e017255c8890ddb
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-08-02 13:43:45 +0900

    Headの設定をインポート

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 
+import { HeadPart } from "../components/HeadPart";
+
 const MyApp = (props: AppProps) => {
   return (
     <ThemeProvider attribute="class">
+      <HeadPart />
       <props.Component {...props.pageProps} />
     </ThemeProvider>
   );
