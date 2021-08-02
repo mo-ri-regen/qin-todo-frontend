7da4a75f8685a48c1fd22c49c4ef9a2aeb4b5984
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-08-02 13:40:27 +0900

    マニフェストの設定とHEADのメタ設定&自動生成ファイルの追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
-};
+});
