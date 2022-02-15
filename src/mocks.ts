31d48e2baaaea60d8ea8c2bbbe30399c5df676e0
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-02-15 10:44:25 +0900

    🤡 Mock APIを作成

---
+++ src/mocks.ts
@@ -0,0 +1,22 @@
+import { rest, setupWorker } from "msw";
+
+const worker = setupWorker(
+  rest.post("/login", (req, res, ctx) => {
+    const isAuthenticated = sessionStorage.getItem("username");
+    if (!isAuthenticated) {
+      return res(
+        ctx.status(403),
+        ctx.json({
+          errorMessage: "Not authenticated",
+        })
+      );
+    }
+    return res(
+      ctx.json({
+        firstName: "John",
+      })
+    );
+  })
+);
+// Register the Service Worker and enable the mocking
+worker.start();
