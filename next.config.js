843b0e7a1f42222feba33782a4364e98d1218aa5
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-02-15 10:32:22 +0900

    ⚗️ pwaのservice workerの更新が煩わしいのでdevでdisable

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = withPWA({
   pwa: {
     dest: "public",
     runtimeCaching,
+    disable: isProd ? false : true,
   },
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
