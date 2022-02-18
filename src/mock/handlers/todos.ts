7a7bfbc52240ce5885515403eb20500f2fa26ab7
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-02-18 21:32:31 +0900

    APIを修正

--- src/mock/handlers/todos.ts
+++ src/mock/handlers/todos.ts
@@ -1,5 +1,5 @@
 import { rest } from "msw";
-import { EXAMPLE_MY_TODO, EXAMPLE_MY_TODO_LIST } from "src/models/todo";
+import { EXAMPLE_MY_TODO } from "src/models/todo";
 
 export const TodosHandlers = [
   // 新しいTodoを作成する
@@ -7,8 +7,18 @@ export const TodosHandlers = [
     return res(ctx.status(201), ctx.json({ id: "1" }));
   }),
 
+  // 特定のTodoの情報を更新する
+  rest.put("http://localhost:8000/todo", (req, res, ctx) => {
+    const { todoId } = req.params;
+    return res(
+      ctx.delay(1000),
+      ctx.status(200),
+      ctx.json({ ...EXAMPLE_MY_TODO, id: todoId })
+    );
+  }),
+
   // 特定のTodoの情報を取得する
-  rest.get("/todos/:todoId", (req, res, ctx) => {
+  rest.get("http://localhost:8000/todo", (req, res, ctx) => {
     const { todoId } = req.params;
     return res(
       ctx.delay(1000),
@@ -18,15 +28,8 @@ export const TodosHandlers = [
   }),
 
   // 特定のTodoを削除する
-  rest.delete("/todos/:todoId", (req, res, ctx) => {
+  rest.delete("http://localhost:8000/todo", (req, res, ctx) => {
     const { todoId } = req.params;
     return res(ctx.delay(1000), ctx.status(200), ctx.json({ id: todoId }));
-  }),
-
-  // 自分または特定のユーザーのメモ一覧を取得する
-  rest.get("/users/:userId/notes", (req, res, ctx) => {
-    const { userId } = req.params;
-    const notes = userId === "my" && EXAMPLE_MY_TODO_LIST;
-    return res(ctx.delay(1000), ctx.status(200), ctx.json(notes));
   }),
 ];
