c9e2f53e69443777ef89eda1bfdb48a565aa1425
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-02-18 21:04:58 +0900

    🏷️ ListTodoの型定義追加

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,9 @@
 export type Todo = { text: string; done?: boolean };
+export type ListTodo = {
+  id: string;
+  text: string;
+  done: boolean;
+};
 export type TodosState = {
   todos: Array<Todo>;
   add: (text: string) => void;
