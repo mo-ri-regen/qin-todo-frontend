e7e828a449aaf7432b92ae3882965c284f1a9ad8
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-02-18 21:05:40 +0900

    🤡 userのhandlerを追加

---
+++ src/mock/handlers/users.ts
@@ -0,0 +1,33 @@
+import { rest } from "msw";
+// import type { UserPutRequest } from "src/models/user";
+import { EXAMPLE_USER_01, EXAMPLE_USER_02 } from "src/models/user";
+
+export const usersHandlers = [
+  // ユーザーを作成する
+  rest.post("/users", (_req, res, ctx) => {
+    return res(ctx.delay(1000), ctx.status(201), ctx.json(EXAMPLE_USER_01));
+  }),
+
+  // 特定のユーザーの情報を取得する
+  rest.get("/users/:userId", (req, res, ctx) => {
+    const { userId } = req.params;
+    return res(
+      ctx.delay(1000),
+      ctx.status(200),
+      ctx.json({ ...EXAMPLE_USER_01, id: userId })
+    );
+  }),
+
+  // 特定のユーザー（自分）の情報を更新する
+  rest.put("/users/:userId", (req, res, ctx) => {
+    const { userId } = req.params;
+    // const body: UserPutRequest = JSON.parse(req.body);
+    // eslint-disable-next-line no-console
+    // console.log({ サーバーが受け取ったリクエスト: body });
+    return res(
+      ctx.delay(1000),
+      ctx.status(200),
+      ctx.json({ ...EXAMPLE_USER_02, id: userId })
+    );
+  }),
+];
