7c5dc3acc51e47edc4e8abb590d0d4b580748fdd
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-02-19 23:56:59 +0900

    ✨ devtools middlewareを導入しました

--- src/libs/store.ts
+++ src/libs/store.ts
@@ -1,27 +1,30 @@
 import type { TodosState } from "src/types";
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 
-const useStore = create<TodosState>((set) => {
-  return {
-    todos: [],
-    add: (text: string) => {
-      return set((state) => {
-        return { todos: [...state.todos, { text: text }] };
-      });
-    },
-    toggleDone: (index: number) => {
-      return set((state) => {
-        return {
-          todos: state.todos.map((todo, id) => {
-            if (index !== id) {
-              return todo;
-            }
-            return { ...todo, done: !todo.done };
-          }),
-        };
-      });
-    },
-  };
-});
+const useStore = create<TodosState>(
+  devtools((set) => {
+    return {
+      todos: [],
+      add: (text: string) => {
+        return set((state) => {
+          return { todos: [...state.todos, { text: text }] };
+        });
+      },
+      toggleDone: (index: number) => {
+        return set((state) => {
+          return {
+            todos: state.todos.map((todo, id) => {
+              if (index !== id) {
+                return todo;
+              }
+              return { ...todo, done: !todo.done };
+            }),
+          };
+        });
+      },
+    };
+  })
+);
 
 export { useStore };
