249a8a9ffbb7f334ff07805ca13fff22800c3f59
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-02-26 12:52:43 +0900

    Loaderの作成とインポート処理

--- src/pages/auth/signin.page.tsx
+++ src/pages/auth/signin.page.tsx
@@ -1,17 +1,16 @@
 import type { NextPage } from "next";
-// import { AuthAction, withAuthUser } from "next-firebase-auth";
-// import { PageLoader } from "src/components/PageLoader";
+import { AuthAction, withAuthUser } from "next-firebase-auth";
 import { Sign } from "src/pages/auth/Sign";
+
+import { Loader } from "./Loader";
 
 const Signin: NextPage = () => {
   return <Sign page="signin" />;
 };
 
-// export default withAuthUser({
-//   whenAuthed: AuthAction.REDIRECT_TO_APP,
-//   whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
-//   whenUnauthedAfterInit: AuthAction.RENDER,
-//   LoaderComponent: PageLoader,
-// })(Signin);
-
-export default Signin;
+export default withAuthUser({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+  whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+  whenUnauthedAfterInit: AuthAction.RENDER,
+  LoaderComponent: Loader,
+})(Signin);
--- src/pages/auth/signup.page.tsx
+++ src/pages/auth/signup.page.tsx
@@ -1,17 +1,16 @@
 import type { NextPage } from "next";
-// import { AuthAction, withAuthUser } from "next-firebase-auth";
-// import { PageLoader } from "src/components/PageLoader";
+import { AuthAction, withAuthUser } from "next-firebase-auth";
 import { Sign } from "src/pages/auth/Sign";
+
+import { Loader } from "./Loader";
 
 const Signup: NextPage = () => {
   return <Sign page="signup" />;
 };
 
-// export default withAuthUser({
-//   whenAuthed: AuthAction.REDIRECT_TO_APP,
-//   whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
-//   whenUnauthedAfterInit: AuthAction.RENDER,
-//   LoaderComponent: PageLoader,
-// })(Signup);
-
-export default Signup;
+export default withAuthUser({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+  whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+  whenUnauthedAfterInit: AuthAction.RENDER,
+  LoaderComponent: Loader,
+})(Signup);
