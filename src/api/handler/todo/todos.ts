75439b4f02ffb33ac6aa281c9f37721066ecc87e
takasaki376 <takasaki.a201070@outlook.jp>
2022-03-06 00:49:15 +0900

    mswの型定義

--- src/api/handler/todo/todos.ts
+++ src/api/handler/todo/todos.ts
@@ -1,32 +1,67 @@
 import { rest } from "msw";
-import { EXAMPLE_MY_TODO, EXAMPLE_MY_TODO_LIST } from "src/models/todo";
+import { EXAMPLE_MY_TODO_LIST } from "src/models/todo";
+import type { ListTodo, PostTodo } from "src/types";
 
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URI}todo/`;
 
 export const TodosHandlers = [
   // 新しいTodoを作成する
-  rest.post(apiUrl, (req, res, ctx) => {
-    return res(ctx.delay(10), ctx.status(201), ctx.json({ id: "6" }));
+  rest.post<PostTodo, never, ListTodo>(apiUrl, (req, res, ctx) => {
+    const { task, sortKey, dueDate, completeDate, isDone } = req.params;
+    return res(
+      ctx.delay(10),
+      ctx.status(201),
+      ctx.json({
+        id: "6",
+        task,
+        userId: "",
+        sortKey,
+        dueDate,
+        completeDate,
+        isDone,
+        createAt: "",
+        updateAt: "",
+      })
+    );
   }),
 
   // 特定のTodoの情報を更新する
-  rest.put(apiUrl, (req, res, ctx) => {
-    const { todoId } = req.params;
+  rest.put<ListTodo, never, ListTodo>(apiUrl, (req, res, ctx) => {
+    const {
+      id,
+      task,
+      userId,
+      sortKey,
+      dueDate,
+      completeDate,
+      isDone,
+      createAt,
+      updateAt,
+    } = req.params;
     return res(
       ctx.delay(10),
       ctx.status(200),
-      ctx.json({ ...EXAMPLE_MY_TODO, id: todoId })
+      ctx.json({
+        id,
+        task,
+        userId,
+        sortKey,
+        dueDate,
+        completeDate,
+        isDone,
+        createAt,
+        updateAt,
+      })
     );
   }),
 
   // 特定のTodoの情報を取得する
-  rest.get(apiUrl, (req, res, ctx) => {
-    // const { todoId } = req.params;
+  rest.get<never, never, ListTodo[]>(apiUrl, (req, res, ctx) => {
     return res(ctx.delay(50), ctx.status(200), ctx.json(EXAMPLE_MY_TODO_LIST));
   }),
 
   // 特定のTodoを削除する
-  rest.delete(apiUrl, (req, res, ctx) => {
+  rest.delete<never, never, Pick<ListTodo, "id">>(apiUrl, (req, res, ctx) => {
     const { todoId } = req.params;
     return res(ctx.delay(10), ctx.status(200), ctx.json({ id: todoId }));
   }),
