8e86f220fceae68b7b5853a22e5446da80c00395
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-05 16:21:51 +0900

    header背景色の削除

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,7 +3,7 @@ import { UserMenu } from "./UserMenu";
 export const Header = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-14 bg-pink-50">
+      <div className="flex justify-center items-center h-14">
         <div className="flex justify-between items-center w-screen">
           <button className="w-9 h-9" />
           <div className="text-3xl">
