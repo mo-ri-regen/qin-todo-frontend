02f09466de3fd828acc6a2dda37336005ced543d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-05 11:54:33 +0900

    🚨 linter errorに対応

--- src/components/TodoToday.tsx
+++ src/components/TodoToday.tsx
@@ -13,9 +13,15 @@ export const TodoTody = memo<Props>((props) => {
   const toggleComplete = useStore((state: TodosState) => {
     return state.toggleDone;
   });
+  const handleToggleComplete = () => {
+    toggleComplete(props.todo.id);
+  };
   const removeTodo = useStore((state: TodosState) => {
     return state.removeTodo;
   });
+  const handleRemoveTodo = () => {
+    return removeTodo(props.todo.id);
+  };
   return (
     <div className="flex items-center mb-4">
       <input
@@ -29,10 +35,7 @@ export const TodoTody = memo<Props>((props) => {
         )}
         type="checkbox"
         checked={props.todo.done}
-        // eslint-disable-next-line react/jsx-handler-names
-        onChange={() => {
-          toggleComplete(props.todo.id);
-        }}
+        onChange={handleToggleComplete}
       />
       <div className={props.todo.done ? "line-through" : ""}>
         {props.todo.text}
@@ -40,9 +43,7 @@ export const TodoTody = memo<Props>((props) => {
       <button
         className="p-1 ml-5 text-gray-100 hover:bg-red-400 rounded"
         // eslint-disable-next-line react/jsx-handler-names
-        onClick={() => {
-          return removeTodo(props.todo.id);
-        }}
+        onClick={handleRemoveTodo}
       >
         <TrashIcon className="w-5 h-5 text-gray-800 dark:text-white " />
       </button>
