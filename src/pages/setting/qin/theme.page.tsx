bcb0129241b254129e9385b5b8127e2991f38340
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-12 11:26:34 +0900

    アイコンと戻るボタンを実装

--- src/pages/setting/qin/theme.page.tsx
+++ src/pages/setting/qin/theme.page.tsx
@@ -1,15 +1,32 @@
 import { RadioGroup } from "@headlessui/react";
-import { CheckIcon } from "@heroicons/react/outline";
+import { CheckIcon, ChevronLeftIcon } from "@heroicons/react/outline";
 import clsx from "clsx";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import { useTheme } from "src/libs/theme";
 
 const SettingMemoTheme: NextPage = () => {
+  const router = useRouter();
   const { themes, isMounted, currentTheme, handleTheme } = useTheme();
   if (!isMounted) return null;
+  const handleClick = () => {
+    return router.back();
+  };
 
   return (
-    <div className="mx-auto w-1/2 whitespace-nowrap">
+    <div className="p-3 mx-auto w-1/2 whitespace-nowrap">
+      <div className="flex items-center pb-6">
+        <button
+          type="button"
+          onClick={handleClick}
+          className="grid place-items-center w-10 h-10 font-bold hover:text-blue-400 focus-visible:text-blue-400 hover:bg-blue-50 focus-visible:bg-blue-50 dark:hover:bg-opacity-10 dark:focus-visible:bg-opacity-10 rounded-full focus-visible:ring-2 focus-visible:ring-blue-400 transition duration-200 ease-in-out focus:outline-none"
+        >
+          <ChevronLeftIcon className="w-6 h-6 text-blue-500" />
+        </button>
+        <div className="flex flex-1 justify-center px-2">
+          <div className="text-xl font-bold">テーマ</div>
+        </div>
+      </div>
       <RadioGroup value={currentTheme} onChange={handleTheme}>
         <RadioGroup.Label className="sr-only">Color theme</RadioGroup.Label>
         {themes.map((theme) => {
