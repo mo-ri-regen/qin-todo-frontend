fa8f89a4fd55b9349a4126920b01831bfa8ac6f0
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-13 19:39:20 +0900

    ポップアップメニュー内のアイコン用スペースを削除

--- src/layout/UserMenu.tsx
+++ src/layout/UserMenu.tsx
@@ -36,13 +36,6 @@ export const UserMenu: VFC = () => {
                   className="absolute left-full xl:-left-full 2xl:left-1/2 z-10 sm:px-0 pl-8 mt-2 w-screen max-w-xs sm:max-w-sm -translate-x-full xl:-translate-x-1/2"
                 >
                   <div className="overflow-hidden py-4 bg-white dark:bg-gray-800 rounded-2xl ring-1 ring-gray-200 dark:ring-gray-700 shadow-lg">
-                    <div>
-                      <Link href="/setting/qin">
-                        <a className="flex items-center p-4 hover:bg-gray-100 focus-visible:bg-gray-100 dark:hover:bg-gray-700 dark:focus-visible:bg-gray-700 focus:outline-none">
-                          <button className="w-9 h-9 bg-blue-500 rounded-full" />
-                        </a>
-                      </Link>
-                    </div>
                     <div className="grid relative">
                       <Link href="/setting/todo">
                         <a className="flex items-center py-2.5 px-4 hover:bg-gray-100 focus-visible:bg-gray-100 dark:hover:bg-gray-700 dark:focus-visible:bg-gray-700 focus:outline-none">
