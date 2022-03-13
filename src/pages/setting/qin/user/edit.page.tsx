a38c34828099dde8ddf85bd3226e43400cb7dc82
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-13 16:46:16 +0900

    editページの作成

---
+++ src/pages/setting/qin/user/edit.page.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+
+const EditPage: NextPage = () => {
+  return (
+    <div className="mx-auto max-w-xl">
+      <div className="flex flex-col flex-1 items-center px-2">
+        <div className="my-4 text-xl font-bold">
+          <h1>EditPage</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditPage;
