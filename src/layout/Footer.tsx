4ff179bf3f6cbf77d8a6d2ebdbd4ce4690d0c331
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-21 11:08:16 +0900

    text field位置調整

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -137,10 +137,10 @@ export const Footer: VFC = () => {
               >
                 <Popover.Panel
                   static
-                  className="absolute -top-9 right-[50%] z-30 translate-x-[50%]"
+                  className="absolute -top-10 right-[50%] z-30 translate-x-[50%]"
                 >
                   <input
-                    className="px-2 mb-3 w-80 h-9 dark:text-gray-700 bg-[#F1F5F9] rounded-full border-none outline-none"
+                    className="px-2 mb-3 w-80 h-8 dark:text-gray-700 bg-[#F1F5F9] rounded-full border-none outline-none"
                     onChange={handleOnChange}
                     value={inputTodo}
                   />
