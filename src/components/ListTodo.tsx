1f3281dea9923d36297f715d8f7c915c30bd874f
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-21 16:36:51 +0900

    モバイル画面で「タスク追加する」ボタンクリック時に、フッダー部分を表示

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -1,6 +1,8 @@
+import { Popover, Transition } from "@headlessui/react";
 import clsx from "clsx";
 import { memo } from "react";
-// import { useState } from "react";
+import { Fragment } from "react";
+import { FooterButtons } from "src/layout/Footer/FooterButtons";
 import { useStore } from "src/libs/store";
 import type { Target, TodosState } from "src/types";
 
@@ -45,57 +47,66 @@ export const ListTodo = memo<Props>((props) => {
     }
   });
 
-  // const [inputTodo, setInputTodo] = useState<string>("");
-
-  // const addTodo = useStore((state) => {
-  //   return state.addTodo;
-  // });
-  // const [error, setError] = useState(" ");
-  // const [isFooterShow, setIsFooterShow] = useState(false);
-  const toggleIsFooterShow = useStore((state) => {
-    return state.toggleIsFooterShow;
-  });
-
-  const handleOnToggleFooter = () => {
-    toggleIsFooterShow();
-  };
-
   return (
-    <div className="lg:min-h-screen">
-      <div
-        className={clsx("mb-3 text-2xl font-semibold", {
-          "text-primary": props.target == "1",
-          "text-secondary": props.target == "2",
-          "text-tertiary": props.target == "3",
-        })}
-      >
-        {props.title}
-      </div>
-      <div className="flex flex-col">
-        <div className="flex items-center">
-          <button
-            className="px-2 mr-2 w-6 h-6 text-white bg-gray-300 rounded-full"
-            onClick={handleOnToggleFooter}
-          >
-            +
-          </button>
-          <div className="text-gray-300">タスクを追加する</div>
-        </div>
-        <div className="overflow-y-auto pt-3 w-full max-h-60 lg:max-h-full">
-          <ol>
-            {todos.map((todo) => {
-              return (
-                <TodoRecord
-                  todo={todo}
-                  key={`todo-${todo.task}-${todo.id}`}
-                  target={props.target}
-                />
-              );
-            })}
-          </ol>
-        </div>
-      </div>
-    </div>
+    <Popover className="lg:min-h-screen">
+      {({ open }) => {
+        return (
+          <>
+            <div
+              className={clsx("mb-3 text-2xl font-semibold", {
+                "text-primary": props.target == "1",
+                "text-secondary": props.target == "2",
+                "text-tertiary": props.target == "3",
+              })}
+            >
+              {props.title}
+            </div>
+            <div className="flex flex-col">
+              <Popover.Button>
+                <div className="flex items-center">
+                  <button className="px-2 mr-2 w-6 h-6 text-white bg-gray-300 rounded-full">
+                    +
+                  </button>
+                  <div className="text-gray-300">タスクを追加する</div>
+                </div>
+              </Popover.Button>
+              <div className="overflow-y-auto pt-3 w-full max-h-60 lg:max-h-full">
+                <ol>
+                  {todos.map((todo) => {
+                    return (
+                      <TodoRecord
+                        todo={todo}
+                        key={`todo-${todo.task}-${todo.id}`}
+                        target={props.target}
+                      />
+                    );
+                  })}
+                </ol>
+              </div>
+            </div>
+            <div className="relative">
+              <Transition
+                show={open}
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 -translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 -translate-y-1"
+              >
+                <Popover.Panel
+                  static
+                  className="lg:hidden fixed right-[50%] bottom-0 z-50 bg-white dark:bg-black translate-x-[50%]"
+                >
+                  <FooterButtons />
+                </Popover.Panel>
+              </Transition>
+            </div>
+          </>
+        );
+      }}
+    </Popover>
   );
 });
 
