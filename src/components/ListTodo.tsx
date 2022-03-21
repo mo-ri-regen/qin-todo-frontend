c7167cf161875f802f45bcba5a4b93e59b3b05d3
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-21 11:56:47 +0900

    overflow時のスクロールを削除

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -81,7 +81,7 @@ export const ListTodo = memo<Props>((props) => {
           </button>
           <div className="text-gray-300">タスクを追加する</div>
         </div>
-        <div className="overflow-y-auto pt-5 my-5 w-full h-full max-h-72">
+        <div className="pt-5 my-5 w-full h-full max-h-72">
           <ol>
             {todos.map((todo) => {
               return (
