6d49c403cad02d97e7c3e40afee2972d7b7786e8
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-23 15:36:26 +0900

    min-h-screenは不要なので削除

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 export const Layout: VFC<Props> = (props) => {
   return (
-    <div className="grid px-3 mx-auto mb-6 h-screen min-h-screen grid-rows-[auto, 1fr]">
+    <div className="grid px-3 mx-auto mb-6 h-screen grid-rows-[auto, 1fr]">
       <Header />
       <main className="grid overflow-y-hidden lg:overflow-y-visible grid-rows-3 lg:grid-rows-1 mb-8">
         {props.children}
