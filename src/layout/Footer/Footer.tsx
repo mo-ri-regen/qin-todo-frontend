cf180572ecc5e06605487a73c9973b43be1ae7c4
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-25 12:48:34 +0900

    Transitionの特に作用していない要素を削除

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import { Popover, Transition } from "@headlessui/react";
 import type { VFC } from "react";
-import { Fragment } from "react";
 
 import { FooterButtons } from "./FooterButtons";
 
@@ -17,16 +16,7 @@ export const Footer: VFC = () => {
             </Popover.Button>
 
             <div className="relative bg-white dark:bg-black">
-              <Transition
-                show={open}
-                as={Fragment}
-                enter="transition ease-out duration-200"
-                enterFrom="opacity-0 -translate-y-1"
-                enterTo="opacity-100 translate-y-0"
-                leave="transition ease-in duration-150"
-                leaveFrom="opacity-100 translate-y-0"
-                leaveTo="opacity-0 -translate-y-1"
-              >
+              <Transition show={open}>
                 <Popover.Panel
                   static
                   className="fixed right-[50%] bottom-0 z-30 bg-white dark:bg-black translate-x-[50%]"
