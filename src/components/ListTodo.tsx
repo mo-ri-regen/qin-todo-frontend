c795f98b8716e4001a494d2187fe252951967849
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-25 12:54:49 +0900

    revert

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -107,7 +107,16 @@ export const ListTodo = memo<Props>((props) => {
           <>
             <List />
             <div className="relative">
-              <Transition show={open}>
+              <Transition
+                show={open}
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 -translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 -translate-y-1"
+              >
                 <Popover.Panel
                   static
                   className="fixed right-[50%] bottom-0 z-50 bg-white dark:bg-gray-900 translate-x-[50%]"
--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { Popover, Transition } from "@headlessui/react";
 import type { VFC } from "react";
+import { Fragment } from "react";
 
 import { FooterButtons } from "./FooterButtons";
 
@@ -16,7 +17,16 @@ export const Footer: VFC = () => {
             </Popover.Button>
 
             <div className="relative bg-white dark:bg-black">
-              <Transition show={open}>
+              <Transition
+                show={open}
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 -translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 -translate-y-1"
+              >
                 <Popover.Panel
                   static
                   className="fixed right-[50%] bottom-0 z-30 bg-white dark:bg-black translate-x-[50%]"
