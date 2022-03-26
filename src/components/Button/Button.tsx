d95ac8af4753708dbb4cd7f40157b933119d83e2
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-26 17:28:12 +0900

    QinMemoのボタンをコピー

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import { forwardRef } from "react";
+
+import type { ButtonType } from "./type";
+import { useButtonClass } from "./useButtonClass";
+
+/**
+ * @package
+ */
+export const Button = forwardRef<HTMLButtonElement, ButtonType>(
+  (props, ref) => {
+    const { children, className, variant, ...rest } = props;
+    const classes = useButtonClass(className, variant);
+    return (
+      <button type="button" ref={ref} {...rest} className={classes}>
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName === "Button";
