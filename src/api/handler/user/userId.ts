d0e2e1919cba988964e5f4f31873c89eb01cd2f5
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-26 16:34:37 +0900

    user処理系はQinMemoをコピー

---
+++ src/api/handler/user/userId.ts
@@ -0,0 +1,32 @@
+import { rest } from "msw";
+import { API_URL } from "src/api/endpoint";
+
+import { EXAMPLE_USER, EXAMPLE_USER_1 } from "./data";
+import type { UserType } from "./type";
+
+const endpoint = `${API_URL}/users/:userId`;
+
+/**
+ * @package 特定のユーザーの情報を取得する
+ */
+export const getUsersUserId = rest.get<never, { userId: string }, UserType>(
+  endpoint,
+  (req, res, ctx) => {
+    const { userId } = req.params;
+    return res(
+      ctx.delay(1000),
+      ctx.status(200),
+      ctx.json({ ...EXAMPLE_USER, id: userId })
+    );
+  }
+);
+
+/**
+ * @package 特定のユーザー（自分）の情報を更新する
+ */
+export const putUsersUserId = rest.put<string, { userId: string }, UserType>(
+  endpoint,
+  (req, res, ctx) => {
+    return res(ctx.delay(1000), ctx.status(200), ctx.json(EXAMPLE_USER_1));
+  }
+);
---
+++ src/api/handler/user/users.ts
@@ -0,0 +1,27 @@
+import { rest } from "msw";
+import { API_URL } from "src/api/endpoint";
+
+import { EXAMPLE_USER } from "./data";
+import type { UserType } from "./type";
+
+const endpoint = `${API_URL}/users`;
+
+/**
+ * @package 自分のユーザー情報を取得する
+ */
+export const getUsers = rest.get<string, never, UserType>(
+  endpoint,
+  (_req, res, ctx) => {
+    return res(ctx.delay(1000), ctx.status(201), ctx.json(EXAMPLE_USER));
+  }
+);
+
+/**
+ * @package ユーザーを作成する
+ */
+export const postUsers = rest.post<string, never, UserType>(
+  endpoint,
+  (_req, res, ctx) => {
+    return res(ctx.delay(1000), ctx.status(201), ctx.json(EXAMPLE_USER));
+  }
+);
