a14120785dc4b68ce9e04ee8e1c9d0c428c4a43b
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-03-26 16:34:37 +0900

    user処理系はQinMemoをコピー

---
+++ src/libs/user/withUser.tsx
@@ -0,0 +1,90 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { AuthAction, useAuthUser, withAuthUser } from "next-firebase-auth";
+import type { ReactNode, VFC } from "react";
+import { useCallback, useEffect, useMemo } from "react";
+import { SWRConfig } from "swr";
+
+import { useUser } from "./useUser";
+
+export const useFetcher = () => {
+  const authUser = useAuthUser();
+  return useCallback(
+    async (key: string) => {
+      const idToken = await authUser.getIdToken();
+      const res = await fetch(key, {
+        headers: { authorization: `Bearer ${idToken}` },
+      });
+      if (!res.ok) {
+        throw new Error();
+      }
+      const json = await res.json();
+      return json;
+    },
+    [authUser]
+  );
+};
+
+const NEW_USER_PAGE = "/setting/qin/user/new";
+const WHITE_LIST_PAGES = ["/user/[userName]", "/memo/[noteId]", "/404"];
+
+const UserController: VFC<{ children: ReactNode }> = (props) => {
+  const router = useRouter();
+  const { user, isLoading } = useUser();
+
+  const isNewUserPage = useMemo(() => {
+    return router.pathname === NEW_USER_PAGE;
+  }, [router.pathname]);
+
+  const isWhiteListPage = useMemo(() => {
+    return WHITE_LIST_PAGES.includes(router.pathname);
+  }, [router.pathname]);
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+    if (user && isNewUserPage) {
+      router.push("/");
+      return;
+    }
+    if (!user && !isNewUserPage) {
+      router.push(NEW_USER_PAGE);
+      return;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading, isNewUserPage, user]);
+
+  if (user || isNewUserPage || isWhiteListPage) {
+    return <>{props.children}</>;
+  }
+
+  return null;
+};
+
+/**
+ * @package
+ */
+export const withUser = (
+  Component: NextPage<any>,
+  options?: Record<string, unknown>
+) => {
+  return withAuthUser(
+    options
+      ? options
+      : {
+          whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+          whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+        }
+  )((props) => {
+    const fetcher = useFetcher();
+
+    return (
+      <SWRConfig value={{ fetcher }}>
+        <UserController>
+          <Component {...props} />
+        </UserController>
+      </SWRConfig>
+    );
+  });
+};
---
+++ src/pages/setting/qin/user/ProfileForm.tsx
@@ -0,0 +1,129 @@
+import { useAuthUser } from "next-firebase-auth";
+import type { VFC } from "react";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { Avatar } from "src/components/Avatar";
+import { Button } from "src/components/Button";
+import { Input } from "src/components/Form/Input";
+import { useUser } from "src/libs/user";
+
+import { useFile } from "./useFile";
+import { useUpsertUser } from "./useUpsertUser";
+
+export type UserForm = { accountName: string; userName: string };
+
+type ProfileFormProps = { accountName?: string; userName?: string };
+
+/**
+ * @package
+ */
+export const ProfileForm: VFC<ProfileFormProps> = () => {
+  const authUser = useAuthUser();
+  const handleSignOut = useCallback(() => {
+    return authUser.signOut();
+  }, [authUser]);
+  const { user } = useUser();
+  const {
+    selectedFile,
+    imageUrl,
+    imageRef,
+    handleChangeFile,
+    handleOpenFileDialog,
+  } = useFile();
+  const { isUpserting, upsertUser } = useUpsertUser(selectedFile);
+  const { register, handleSubmit, formState } = useForm<UserForm>({
+    defaultValues: {
+      accountName: user?.accountName ?? authUser.displayName ?? "",
+      userName: user?.userName ?? "",
+    },
+  });
+
+  return (
+    <form onSubmit={handleSubmit(upsertUser)}>
+      <div className="space-y-6 sm:space-y-8">
+        <div>
+          <div className="flex justify-start items-center space-x-6">
+            <Avatar
+              noDialog
+              src={imageUrl ?? user?.avatarUrl ?? authUser.photoURL ?? ""}
+              alt={user?.accountName}
+              width={96}
+              height={96}
+              className="w-24 h-24"
+            />
+            <input
+              ref={imageRef}
+              type="file"
+              className="hidden"
+              onChange={handleChangeFile}
+              accept="image/png, image/jpeg"
+            />
+            <Button
+              variant="solid-gray"
+              className="py-2.5 px-5 mt-4"
+              onClick={handleOpenFileDialog}
+            >
+              アイコンを{user ? "変更する" : "設定する"}
+            </Button>
+          </div>
+        </div>
+
+        <Input
+          label="名前"
+          {...register("accountName", {
+            required: { value: true, message: "入力必須です" },
+            maxLength: { value: 64, message: "64文字以下にする必要があります" },
+            minLength: { value: 2, message: "2文字以上にする必要があります" },
+          })}
+          error={formState.errors.accountName?.message}
+        />
+
+        <Input
+          label="ユーザー名"
+          prefix="@"
+          {...register("userName", {
+            required: { value: true, message: "入力必須です" },
+            maxLength: { value: 16, message: "16文字以下にする必要があります" },
+            minLength: { value: 4, message: "4文字以上にする必要があります" },
+            pattern: {
+              value: /^[A-Za-z0-9_]+$/i,
+              message: "半角英数字と_だけご使用できます",
+            },
+          })}
+          error={formState.errors.userName?.message}
+        />
+      </div>
+
+      <div className="mt-12 space-y-4">
+        {user ? (
+          <Button
+            type="submit"
+            variant="solid-blue"
+            className="p-3 w-full"
+            disabled={isUpserting}
+          >
+            保存する
+          </Button>
+        ) : (
+          <>
+            <Button
+              type="submit"
+              variant="solid-blue"
+              className="p-3 w-full"
+              disabled={isUpserting}
+            >
+              登録してはじめる
+            </Button>
+            <Button
+              variant="solid-gray"
+              className="p-3 w-full"
+              onClick={handleSignOut}
+            >
+              登録せずに終了する
+            </Button>
+          </>
+        )}
+      </div>
+    </form>
+  );
+};
