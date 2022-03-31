5eb57a2e878e6e94771b097ee88c36d23247b58e
takasaki376 <takasaki.a201070@outlook.jp>
2022-03-31 22:29:01 +0900

    並び替え処理

--- src/components/ListTodo.tsx
+++ src/components/ListTodo.tsx
@@ -1,3 +1,8 @@
+import { useDroppable } from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 import { memo, useState } from "react";
 import { getStringFromDate } from "src/libs/dateFunc";
 import { selectTodos, useStore } from "src/libs/store";
@@ -20,6 +25,9 @@ export const ListTodo = memo<Props>((props) => {
   const strDate = getStringFromDate(date);
 
   const todos = selectTodos(allTodos, strDate, props.target);
+  const todoIds = todos.map((todoTask) => {
+    return String(todoTask.id);
+  });
   const [isInput, setIsInput] = useState<boolean>(false);
 
   const AddPcTaskButton = () => {
@@ -41,19 +49,42 @@ export const ListTodo = memo<Props>((props) => {
     );
   };
 
+  const { setNodeRef } = useDroppable({
+    id: props.target,
+    data: {
+      type: "container",
+      children: todos,
+    },
+  });
+
   return (
     <div className="pt-3 w-full">
       <ol>
-        {todos.map((todo) => {
-          return (
-            <TodoRecord
-              todo={todo}
-              key={`todo-${todo.task}-${todo.id}`}
-              target={props.target}
-            />
-          );
-        })}
-        <AddPcTaskButton />
+        <div
+          ref={setNodeRef}
+          style={
+            {
+              "--columns": 1,
+            } as React.CSSProperties
+          }
+        >
+          <SortableContext
+            id={props.target}
+            items={todoIds}
+            strategy={verticalListSortingStrategy}
+          >
+            {todos.map((todo) => {
+              return (
+                <TodoRecord
+                  todo={todo}
+                  key={`todo-${todo.task}-${todo.id}`}
+                  target={props.target}
+                />
+              );
+            })}
+            <AddPcTaskButton />
+          </SortableContext>
+        </div>
       </ol>
     </div>
   );
--- src/components/TodoRecord.tsx
+++ src/components/TodoRecord.tsx
@@ -1,3 +1,5 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { DocumentDuplicateIcon, TrashIcon } from "@heroicons/react/outline";
 import clsx from "clsx";
 import { memo, useRef } from "react";
@@ -28,44 +30,53 @@ export const TodoRecord = memo<Props>((props) => {
   };
   const focusRef = useRef(null);
 
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.todo.id });
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
   return (
-    <div className="group flex justify-between items-center mr-5 mb-4">
-      <div className="flex items-center ">
-        <input
-          className={clsx(
-            "mr-4 w-6 h-6 rounded-full ring-0 focus:ring-gray-400",
-            {
-              "text-primary": props.target === "today",
-              "text-secondary": props.target === "nextday",
-              "text-tertiary": props.target === "other",
-            }
-          )}
-          type="checkbox"
-          checked={props.todo.isDone}
-          onClick={handleToggleComplete}
-        />
-        <div
-          ref={focusRef}
-          tabIndex={-1}
-          className={clsx(
-            "px-6 m-0 my-auto w-full dark:bg-gray-700 dark:focus:bg-transparent rounded-full border-none focus:ring-blue-300 cursor-text",
-            {
-              "line-through": props.todo.isDone,
-            }
-          )}
-        >
-          {props.todo.task}
+    <li ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <div className="group flex justify-between items-center mr-5 mb-4">
+        <div className="flex items-center ">
+          <input
+            className={clsx(
+              "mr-4 w-6 h-6 rounded-full ring-0 focus:ring-gray-400",
+              {
+                "text-primary": props.target === "today",
+                "text-secondary": props.target === "nextday",
+                "text-tertiary": props.target === "other",
+              }
+            )}
+            type="checkbox"
+            checked={props.todo.isDone}
+            onClick={handleToggleComplete}
+          />
+          <div
+            ref={focusRef}
+            tabIndex={-1}
+            className={clsx(
+              "px-6 m-0 my-auto w-full dark:bg-gray-700 dark:focus:bg-transparent rounded-full border-none focus:ring-blue-300 cursor-text",
+              {
+                "line-through": props.todo.isDone,
+              }
+            )}
+          >
+            {props.todo.task}
+          </div>
+        </div>
+        <div className="flex w-1/6 opacity-10 group-hover:opacity-100">
+          <button className="p-1 ml-5" onClick={handleDupulicateTodo}>
+            <DocumentDuplicateIcon className="w-5 h-5 text-gray-500 dark:text-white" />
+          </button>
+          <button className="p-1 sm:mx-3" onClick={handleRemoveTodo}>
+            <TrashIcon className="w-5 h-5 text-gray-500 dark:text-white" />
+          </button>
         </div>
       </div>
-      <div className="flex w-1/6 opacity-10 group-hover:opacity-100">
-        <button className="p-1 ml-5" onClick={handleDupulicateTodo}>
-          <DocumentDuplicateIcon className="w-5 h-5 text-gray-500 dark:text-white" />
-        </button>
-        <button className="p-1 sm:mx-3" onClick={handleRemoveTodo}>
-          <TrashIcon className="w-5 h-5 text-gray-500 dark:text-white" />
-        </button>
-      </div>
-    </div>
+    </li>
   );
 });
 
