c9b7dcdb9076770dc01eb1370ad07d4f7eac9c0a
takasaki376 <takasaki.a201070@outlook.jp>
2022-03-31 22:19:36 +0900

    storeの編集

--- src/libs/store.ts
+++ src/libs/store.ts
@@ -1,9 +1,10 @@
+import { arrayMove } from "@dnd-kit/sortable";
 import axios from "axios";
 import type { ListTodo, PostTodo, Target, TodosState } from "src/types";
 import create from "zustand";
 import { devtools } from "zustand/middleware";
 
-import { getToday } from "./dateFunc";
+import { getToday, getTommorow } from "./dateFunc";
 
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URI}todo/`;
 
@@ -46,6 +47,9 @@ const useStore = create<TodosState>(
   devtools((set) => {
     return {
       todos: [],
+      activeId: null,
+      activeTarget: null,
+      orveTarget: null,
       editTodo: initEditTodo,
       getTodos: async () => {
         const response = await axios.get<ListTodo[]>(apiUrl);
@@ -140,6 +144,232 @@ const useStore = create<TodosState>(
               isDone: postTodo.isDone,
             },
           };
+        });
+      },
+      setActiveId: (id: string) => {
+        return set((state) => {
+          const todo = state.todos.find((todo) => {
+            todo.id === id;
+          });
+          let target: Target | null = state.activeId;
+          const dueDate = todo?.dueDate;
+          const today = String(getToday().toString);
+          const nextday = String(getTommorow().toString);
+
+          switch (dueDate) {
+            case today:
+              target = "today";
+              break;
+            case nextday:
+              target = "nextday";
+              break;
+            default:
+              target = "other";
+              break;
+          }
+          return { activeId: target };
+        });
+      },
+      findTarget: (id: string, isActive: boolean) => {
+        return set((state) => {
+          const todo = state.todos.find((todo) => {
+            return todo.id === id;
+          });
+          if (!todo) {
+            if (isActive) {
+              return { activeTarget: null, orveTarget: state.orveTarget };
+            } else {
+              return { activeTarget: state.activeTarget, orveTarget: null };
+            }
+          }
+
+          let activeTarget: Target | null = state.activeTarget;
+          let orveTarget: Target | null = state.orveTarget;
+          if (!isActive && id === "") {
+            orveTarget = null;
+          } else {
+            const dueDate = todo?.dueDate;
+            const today = getToday();
+            const nextday = getTommorow();
+
+            switch (dueDate) {
+              case today:
+                if (isActive) {
+                  activeTarget = "today";
+                } else {
+                  orveTarget = "today";
+                }
+                break;
+              case nextday:
+                if (isActive) {
+                  activeTarget = "nextday";
+                } else {
+                  orveTarget = "nextday";
+                }
+                break;
+              default:
+                if (isActive) {
+                  activeTarget = "other";
+                } else {
+                  orveTarget = "other";
+                }
+                break;
+            }
+          }
+          return { activeTarget: activeTarget, orveTarget: orveTarget };
+        });
+      },
+      taskDropOver: (id: string, overId: string, strDate: string) => {
+        return set((state) => {
+          const updateTodos: ListTodo[] = state.todos;
+          // 移動先が無い場合は、何もせずに終了する
+          if (!overId || overId === "void") {
+            return { todos: updateTodos, activeId: null };
+          }
+          // 移動元、移動先のコンテナが変わっていない場合は、何もせずに終了する
+          if (
+            !state.activeTarget ||
+            !state.orveTarget ||
+            state.activeTarget === state.orveTarget
+          ) {
+            return { todos: updateTodos, activeId: null };
+          }
+
+          const activeTarget: Target = state.activeTarget;
+          const orveTarget: Target = state.orveTarget;
+
+          // 移動元コンテナ
+          const activeItems = selectTodos(state.todos, strDate, activeTarget);
+          // 移動先コンテナ
+          const overItems = selectTodos(state.todos, strDate, orveTarget);
+
+          // 移動先コンテナの位置
+          let activeIndex = activeItems.findIndex((item) => {
+            return item.id === id;
+          });
+
+          // 移動先コンテナの位置
+          let overIndex = overItems.findIndex((item) => {
+            return item.id === overId;
+          });
+          const newIndex = overIndex + 1;
+
+          // 移動対象のタスク
+          const newItem = activeItems.find((item) => {
+            return item.id === id;
+          });
+          if (!newItem) {
+            return { todos: updateTodos, activeId: null };
+          }
+          switch (orveTarget) {
+            case "today":
+              newItem.dueDate = getToday();
+              break;
+            case "nextday":
+              newItem.dueDate = getTommorow();
+              break;
+            case "other":
+              newItem.dueDate = "";
+              break;
+            default:
+              break;
+          }
+
+          // 移動元コンテナから、移動対象を除く
+          const activeContainer = activeItems.filter((item) => {
+            return item.id !== id;
+          });
+          for (
+            let index = activeIndex;
+            index < activeContainer.length;
+            index += 1
+          ) {
+            activeContainer[index].sortKey = activeIndex + 1;
+            activeIndex += 1;
+          }
+
+          // 移動先コンテナに、移動対象を追加する
+          const overContainer = overItems.slice(0, newIndex);
+          overContainer.push(newItem);
+
+          const overContainerBk = overItems.slice(newIndex, overItems.length);
+          for (let index = 0; index < overContainerBk.length; index += 1) {
+            overContainerBk[index].sortKey = overIndex + 1;
+            overContainer.push(overContainerBk[index]);
+            overIndex += 1;
+          }
+          // 移動元の並びを全体のstateに反映する
+          activeContainer.map((todo) => {
+            updateTodos.find((upTodo) => {
+              return upTodo.id === todo.id;
+            })
+              ? todo
+              : updateTodos;
+          });
+          // 移動先の並びを全体のstateに反映する
+          overContainer.map((todo) => {
+            updateTodos.find((upTodo) => {
+              return upTodo.id === todo.id;
+            })
+              ? todo
+              : updateTodos;
+          });
+
+          return { todos: updateTodos, activeId: null };
+        });
+      },
+      taskDropEnd: (id: string, overId: string, strDate: string) => {
+        return set((state) => {
+          const updateTodos: ListTodo[] = state.todos;
+
+          if (!state.activeTarget) {
+            return { todos: updateTodos, activeId: null };
+          }
+
+          const activeTarget: Target = state.activeTarget;
+
+          // 移動したコンテナを取得する
+          const items = selectTodos(state.todos, strDate, activeTarget);
+
+          // 移動したタスクの移動前の位置
+          const activeIndex = items.findIndex((item) => {
+            return item.id === id;
+          });
+          // 移動したタスクの移動後の位置
+          const overIndex = items.findIndex((item) => {
+            return item.id === overId;
+          });
+
+          let activeContainer: ListTodo[] = [];
+          if (activeIndex !== overIndex) {
+            activeContainer = arrayMove(items, activeIndex, overIndex);
+            let sortIndex = 0;
+            if (activeIndex < overIndex) {
+              sortIndex = activeIndex;
+            } else {
+              sortIndex = overIndex;
+            }
+            for (
+              let index = sortIndex;
+              index < activeContainer.length;
+              index += 1
+            ) {
+              activeContainer[index].sortKey = sortIndex + 1;
+              sortIndex += 1;
+            }
+          } else {
+            activeContainer = items;
+          }
+          // 移動後の並びを全体のstateに反映する
+          activeContainer.map((todo) => {
+            updateTodos.find((upTodo) => {
+              return upTodo.id === todo.id;
+            })
+              ? todo
+              : updateTodos;
+          });
+
+          return { todos: updateTodos, activeId: null };
         });
       },
     };
