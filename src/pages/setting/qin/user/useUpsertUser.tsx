0dddf83d1661fb39370387507faf8652e66853d8
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-04-02 16:08:18 +0900

    ⚰️ removed unused code

--- src/pages/setting/qin/user/useUpsertUser.tsx
+++ src/pages/setting/qin/user/useUpsertUser.tsx
@@ -1,76 +1,22 @@
 import { ref, uploadBytesResumable } from "firebase/storage";
-// import { useRouter } from "next/router";
 import { useAuthUser } from "next-firebase-auth";
 import { useCallback, useState } from "react";
 import toast from "react-hot-toast";
-// import { API_URL } from "src/api/endpoint";
-// import { isUserType } from "src/api/handler/user/type";
 import { storage } from "src/libs/auth/initAuth";
-import { useUser } from "src/libs/user";
-
-// import type { UserForm } from "./TodoProfileForm";
-
-// const createAvatarUrl = (uid: string) => {
-//   const filePath = encodeURIComponent(`thumbnails/${uid}_200x200`);
-//   return `${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_URL}/${filePath}?alt=media`;
-// };
-
-// const sleep = (ms: number) => {
-//   return new Promise((resolve) => {
-//     return setTimeout(resolve, ms);
-//   });
-// };
 
 /**
  * @package
  */
 export const useUpsertUser = (selectedFile?: File) => {
-  // const router = useRouter();
   const authUser = useAuthUser();
 
-  const { user } = useUser();
   const [isUpserting, setIsUpserting] = useState(false);
 
   const onSubmit = useCallback(async () => {
-    //   if (!authUser.id) {
-    //     await router.push("/auth/signin");
-    //     throw new Error("再度ログインする必要があります");
-    //   }
-
     // 画像の登録処理
     if (!selectedFile) return;
     const sotrageRef = ref(storage, `thumbnails/${authUser.id}`);
     uploadBytesResumable(sotrageRef, selectedFile);
-    // TODO: 現在画像のリサイズに時間がかかっており、表示できなくなるため、あえて2秒待機させる
-    //   await sleep(2000);
-    // }
-
-    // ユーザー情報の更新処理
-    // const idToken = await authUser.getIdToken();
-    // const res = await fetch(
-    //   user ? `${API_URL}/users/${user.userName}` : `${API_URL}/users`,
-    //   {
-    //     method: user ? "PUT" : "POST",
-    //     headers: {
-    //       authorization: `Bearer ${idToken}`,
-    //       "content-type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       ...formData,
-    //       uid: authUser.id,
-    //       avatarUrl: selectedFile
-    //         ? createAvatarUrl(authUser.id)
-    //         : user?.avatarUrl ?? authUser.photoURL,
-    //     }),
-    //   }
-    // );
-
-    // レスポンスの処理
-    // const data = await res.json();
-    // if (!isUserType(data)) {
-    //   throw new Error();
-    // }
-    // return data;
   }, [authUser, selectedFile]);
 
   const upsertUser = useCallback(async () => {
@@ -78,21 +24,15 @@ export const useUpsertUser = (selectedFile?: File) => {
     await toast.promise(onSubmit(), {
       loading: "処理中",
       success: () => {
-        // setUser(data);
         setIsUpserting(false);
-        return user ? "保存しました" : "登録しました";
+        return authUser ? "保存しました" : "登録しました";
       },
       error: (error) => {
         setIsUpserting(false);
         return error.message ?? "失敗しました";
       },
     });
-
-    // 新規登録の場合はリダイレクト
-    // if (!user) {
-    //   await router.push("/");
-    // }
-  }, [onSubmit, user]);
+  }, [onSubmit, authUser]);
 
   return { isUpserting, upsertUser };
 };
