4ed5eff87c22f3a603fcaabeafa1618a4f2f0e70
takasaki376 <takasaki.a201070@outlook.jp>
2022-04-09 16:05:48 +0900

    フッダーのボタン表示制御追加

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -4,6 +4,9 @@ import { initEditTodo, useStore } from "src/libs/store";
 import { FooterButtons } from "./FooterButtons";
 
 export const Footer: VFC = () => {
+  const isAddInput = useStore((state) => {
+    return state.isAddInput;
+  });
   const setIsAddInput = useStore((state) => {
     return state.setIsAddInput;
   });
@@ -16,14 +19,16 @@ export const Footer: VFC = () => {
   };
   return (
     <div className="grid lg:hidden relative">
-      <div className="flex fixed right-0 bottom-0 left-0 z-30 flex-row mx-auto max-w-sm bg-white dark:bg-gray-900 ">
-        <div className="w-full">
-          <button
-            className="w-full h-8 dark:text-gray-700 bg-[#F1F5F9] rounded-full border-none outline-none"
-            onClick={handleOpenModal}
-          />
+      {isAddInput && (
+        <div className="flex fixed right-0 bottom-0 left-0 z-30 flex-row mx-auto max-w-sm bg-white dark:bg-gray-900 ">
+          <div className="w-full">
+            <button
+              className="w-full h-8 dark:text-gray-700 bg-[#F1F5F9] rounded-full border-none outline-none"
+              onClick={handleOpenModal}
+            />
+          </div>
         </div>
-      </div>
+      )}
       <FooterButtons />
     </div>
   );
