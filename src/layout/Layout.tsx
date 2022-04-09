08a2f382d2ee50876a211cd924148f3c407fee05
takasaki376 <takasaki.a201070@outlook.jp>
2022-04-09 22:04:22 +0900

    フッダーのボタン入力時の対応

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -8,9 +8,11 @@ type Props = {
 };
 export const Layout: VFC<Props> = (props) => {
   return (
-    <div className="grid px-3 mx-auto mb-6 min-h-screen grid-rows-[auto, 1fr]">
-      <Header />
-      <main className="overflow-auto h-[90vh]">{props.children}</main>
+    <div>
+      <div className="grid px-3 mx-auto mb-6 min-h-screen grid-rows-[auto, 1fr]">
+        <Header />
+        <main className="overflow-auto h-[90vh]">{props.children}</main>
+      </div>
       <Footer />
     </div>
   );
