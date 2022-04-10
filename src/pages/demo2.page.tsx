e8eee63cde57f52448ffad934730305d4801b8bd
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-04-10 21:58:22 +0900

    react-swipeable-viewsのサンプルコード

---
+++ src/pages/demo2.page.tsx
@@ -0,0 +1,36 @@
+import SwipeableViews from "react-swipeable-views";
+
+const styles = {
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: "#fff",
+  },
+  slide1: {
+    backgroundColor: "#FEA900",
+  },
+  slide2: {
+    backgroundColor: "#B3DC4A",
+  },
+  slide3: {
+    backgroundColor: "#6AC0FF",
+  },
+};
+
+const Demo2 = () => {
+  return (
+    <SwipeableViews enableMouseEvents>
+      <div style={Object.assign({}, styles.slide, styles.slide1)}>
+        slide n°1
+      </div>
+      <div style={Object.assign({}, styles.slide, styles.slide2)}>
+        slide n°2
+      </div>
+      <div style={Object.assign({}, styles.slide, styles.slide3)}>
+        slide n°3
+      </div>
+    </SwipeableViews>
+  );
+};
+
+export default Demo2;
