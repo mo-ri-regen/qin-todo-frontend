c5f7ba544d574c667650b03e02d74698c3ec4676
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-04-13 15:46:02 +0900

    images.domainsを設定

--- next.config.js
+++ next.config.js
@@ -12,4 +12,9 @@ module.exports = withPWA({
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   pageExtensions: ["page.tsx", "page.ts"],
+  images: {
+    domains: [
+      'https://firebasestorage.googleapis.com/v0/b/clerk-projects.appspot.com/o/user%2FEZN9sL51EDRMueJlLfuNfZ8n3kJ3%2Fsetting%2Fprofile%2FEZN9sL51EDRMueJlLfuNfZ8n3kJ3?alt=media&token=37c4142f-5df8-4ada-8bb5-09c72f49ed60) on `next/image`, hostname "firebasestorage.googleapis.com',
+    ],
+  },
 });
